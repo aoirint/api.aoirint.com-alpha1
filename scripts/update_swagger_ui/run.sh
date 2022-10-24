1f6b28ebd2bb7787a7ef6d01dff4e3e5539507c6
aoirint <aoirint@gmail.com>
2022-10-24 12:43:39 +0900

    fullpath

--- scripts/update_swagger_ui/run.sh
+++ scripts/update_swagger_ui/run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 set -eu
-
-BASE_ROOT=$(realpath $(dirname $0))
-OUTPUT_ROOT=${BASE_ROOT}/../../public/docs
+OUTPUT_ROOT=/home/aoi/apps/api.aoirint.com/public/docs
 
 docker buildx build . -t update_swagger_ui
 docker run --rm -e VERSION="v4.15.0" -e OUTPUT_ROOT=/dist -e JSON_URL="https://api.aoirint.com/openapi.json" -v "${OUTPUT_ROOT}:/dist" update_swagger_ui
