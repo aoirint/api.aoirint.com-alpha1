199474c93e7175c8d655447d81c54e3bc8aefd6e
aoirint <aoirint@gmail.com>
2022-10-24 12:48:41 +0900

    remove unused JSON_API

--- scripts/update_swagger_ui/run.sh
+++ scripts/update_swagger_ui/run.sh
@@ -4,4 +4,4 @@ set -eu
 OUTPUT_ROOT=/home/aoi/apps/api.aoirint.com/public/docs
 
 docker buildx build . -t update_swagger_ui
-docker run --rm -e VERSION="v4.15.0" -e OUTPUT_ROOT=/dist -e JSON_URL="https://api.aoirint.com/openapi.json" -v "${OUTPUT_ROOT}:/dist" update_swagger_ui
+docker run --rm -e VERSION="v4.15.0" -e OUTPUT_ROOT=/dist -v "${OUTPUT_ROOT}:/dist" update_swagger_ui
